990a978a60e4511bb2e879c90efb1166bdcf19b2
evandrvalente <evandr.valente@gmail.com>
2022-10-27 21:57:50 -0300

    feat: soma_de_numero.py

--- counter.py
+++ counter.py
@@ -4,16 +4,16 @@ from collections import Counter
 number_list = []
 
 for x in range(10000):
-    number_list.append(random.randint(0,1000))
+    number_list.append(random.randint(0, 1000))
 
-#print(number_list)
+# print(number_list)
 
 counter1 = Counter(number_list)
-#print(counter1)
-#print(counter1[13])
+# print(counter1)
+# print(counter1[13])
 
 most_commons = counter1.most_common()
-#print(most_commons)
+# print(most_commons)
 
 elemento_mais_comum, numero_de_vezes = most_commons[0]
 
---
+++ draw_triangle.py
@@ -0,0 +1,5 @@
+def draw_triangle(n):
+    for row in range(1, n + 1):
+        print(row * '*')
+
+draw_triangle(9)
